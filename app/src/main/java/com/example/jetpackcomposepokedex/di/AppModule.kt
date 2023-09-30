706b8f0efea732efc25ee4144733435ddcdd15d9
Ali Abd <111752624+aliboss755@users.noreply.github.com>
2023-09-30 21:04:56 +0300

    // Retrofit Setup

--- app/src/main/java/com/example/jetpackcomposepokedex/di/AppModule.kt
+++ app/src/main/java/com/example/jetpackcomposepokedex/di/AppModule.kt
@@ -1,11 +1,32 @@
 package com.example.jetpackcomposepokedex.di
 
+import com.example.jetpackcomposepokedex.data.remote.PokeApi
+import com.example.jetpackcomposepokedex.repository.PokemonRepository
+import com.example.jetpackcomposepokedex.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
+    @Singleton
+    @Provides
+ fun providePokemonRepository(
+     api: PokeApi
+ )=PokemonRepository(api)
+    @Singleton
+    @Provides
+    fun providePokemonApi():PokeApi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create()
+    }
 }
---
+++ app/src/main/java/com/example/jetpackcomposepokedex/util/Resource.kt
@@ -0,0 +1,9 @@
+package com.example.jetpackcomposepokedex.util
+
+import android.os.Message
+
+sealed class Resource<T> (val data : T? , val message: String? =null){
+    class Success<T> ( data: T) : Resource<T>(data)
+    class Loading<T> ( data: T?=null) : Resource<T>(data)
+    class Error<T> (data: T?=null, message: String) : Resource<T>(data,message)
+}
