6db6316c09698930deb9dcafacdb7d97bffd84f4
Ali Abd <111752624+aliboss755@users.noreply.github.com>
2023-09-30 22:07:48 +0300

    // SearchBar and PokemonListScreen

--- app/src/main/java/com/example/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepokedex/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.jetpackcomposepokedex.PokemonList.PokemonListScreen
 import com.example.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen"
                 ) {
                     composable("pokemon_list_screen"){
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable(
                         "pokemon_detail_screen/{dominantColor}/{pokemonName}",
